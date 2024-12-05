bf8b99dd97e7c92db10741762b9c17993fc272f1
sagarmali19 <sagarmali.work@gmail.com>
2024-12-05 11:41:25 +0530

    basic java programming

---
+++ basic-java/Hello.java
@@ -0,0 +1,32 @@
+class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World !!");
+
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+
+        // data types
+        byte b1 = 127;
+        short sh1 = 558;
+        long l1 = 231231231L;
+        float f1 = 22.3f;
+        double d1 = 22.3;
+
+        char c = 'k';
+        boolean b = true;
+
+        // type conversion and casting
+        byte b2 = 127;
+        int a = b2;
+        System.out.println(a);
+        byte k2 = (byte)a;
+        System.out.println(k2);
+
+        // performing modulo
+        int a2 = 257;
+        byte k3 = (byte) a2;
+        System.out.println(k3);
+    }
+}
