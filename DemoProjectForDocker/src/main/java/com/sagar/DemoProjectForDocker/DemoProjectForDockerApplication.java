eae7976bccd45bad372c5aa3f35786992125ebb3
sagarmali19 <sagarmali.work@gmail.com>
2025-04-18 10:26:54 +0530

    docker project

---
+++ DemoProjectForDocker/src/main/java/com/sagar/DemoProjectForDocker/DemoProjectForDockerApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.DemoProjectForDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForDockerApplication.class, args);
+	}
+
+}
---
+++ DemoProjectForDocker/src/test/java/com/sagar/DemoProjectForDocker/DemoProjectForDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.DemoProjectForDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
